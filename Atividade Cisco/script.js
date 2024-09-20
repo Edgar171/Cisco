0a0d3a0ee3701a63826ae8ca794e004b4fac3de8
Edgar171 <85360338+Edgar171@users.noreply.github.com>
2024-09-20 09:27:53 -0300

    Add files via upload

--- Atividade Cisco/script.js
+++ Atividade Cisco/script.js
@@ -3,6 +3,6 @@ function calcular(){
     var Q2 = parseFloat(document.getElementById('Q2').value);
     var D = parseFloat(document.getElementById('D').value);
 
-    var Força = (9000000 * Q1 * Q2 / (D * D));
+    var Força = ( 9000000000 * Q1 * Q2 / (D * D));
     document.getElementById('resultado').innerText = Força;
 }
