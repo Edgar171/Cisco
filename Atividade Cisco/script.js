b767d4689d696194498c8c5f72f87b69294b90da
Edgar171 <85360338+Edgar171@users.noreply.github.com>
2024-09-20 09:43:55 -0300

    Delete Atividade Cisco directory

--- Atividade Cisco/script.js
+++
@@ -1,8 +0,0 @@
-function calcular(){
-    var Q1 = parseFloat(document.getElementById('Q1').value);
-    var Q2 = parseFloat(document.getElementById('Q2').value);
-    var D = parseFloat(document.getElementById('D').value);
-
-    var Força = ( 9000000000 * Q1 * Q2 / (D * D));
-    document.getElementById('resultado').innerText = Força;
-}
