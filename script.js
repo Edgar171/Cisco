c789825ed8ffdaab333e386922a5fa98b1d12677
Edgar171 <85360338+Edgar171@users.noreply.github.com>
2024-09-20 09:43:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcular(){
+    var Q1 = parseFloat(document.getElementById('Q1').value);
+    var Q2 = parseFloat(document.getElementById('Q2').value);
+    var D = parseFloat(document.getElementById('D').value);
+
+    var Força = ( 9000000000 * Q1 * Q2 / (D * D));
+    document.getElementById('resultado').innerText = Força;
+}
